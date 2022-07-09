610ddad110510caf0604dc0cf5c5ce905e4df15f
Sean Zhu <interestinglythere@gmail.com>
2022-07-08 19:48:50 -0700

    Abstract

--- extension/page-background.js
+++ extension/page-background.js
@@ -1,133 +1,90 @@
+/**
+ * @param {any[]} args
+ */
+function workerLog(...args) {
+  // eslint-disable-next-line no-console
+  console.info("Pagefreeze:", ...args);
+}
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.webNavigation.onCommitted.addListener((details) => {
     if (details.frameId !== 0) return;
 
-    console.log(details.frameId, details.tabId);
+    {
+      let { tabId, frameId } = details;
+      workerLog("chrome.webNavigation.onCommitted", { tabId, frameId });
+    }
 
-    console.log(chrome.scripting);
     chrome.scripting.executeScript(
       {
         target: { tabId: details.tabId },
+        // @ts-expect-error: This property hasn't been added to @types/chrome.
         injectImmediately: true,
         world: "MAIN",
         func: () => {
-          // let { addEventListener } = window;
+          /**
+           * @param {any[]} args
+           */
+          function pageLog(...args) {
+            // eslint-disable-next-line no-console
+            console.info("Pagefreeze:", ...args);
+          }
 
-          window.addEventListener =
-            // @ts-expect-error
-            (type, listener, options) => {
-              console.log("Pagefreeze window.addEventListener");
-              // console.log("window.addEventListener", type, listener, options);
-              // return addEventListener.call(window, type, listener, options);
-            };
+          /**
+           * @param {Record<string, any>} obj
+           * @param {string} objName
+           * @param {Record<string, any>} methodMap
+           */
+          function assignDummyMethods(obj, objName, methodMap) {
+            for (let [key, value] of Object.entries(methodMap)) {
+              // eslint-disable-next-line no-param-reassign
+              obj[key] = () => {
+                pageLog(`Prevented ${objName}.${key}`);
+                return value;
+              };
+            }
+          }
 
-          // @ts-expect-error
-          window.setTimeout = () => {
-            console.log("Pagefreeze window.setTimeout");
+          assignDummyMethods(window, "window", {
+            addEventListener: undefined,
+            setTimeout: undefined,
+            setInterval: undefined,
+            fetch: Promise.resolve(),
+          });
+
+          assignDummyMethods(window.document, "document", {
+            addEventListener: undefined,
+          });
+
+          assignDummyMethods(Element.prototype, "Element.prototype", {
+            addEventListener: undefined,
+          });
+
+          XMLHttpRequest.constructor = function() {
+            pageLog("XMLHttpRequest");
+            assignDummyMethods(this, "XMLHttpRequest", {
+              open: undefined,
+              setRequestHeader: undefined,
+              send: undefined,
+              addEventListener: undefined,
+            });
           };
 
-          // @ts-expect-error
-          window.setInterval = () => {
-            console.log("Pagefreeze window.setInterval");
+          IntersectionObserver.constructor = function() {
+            pageLog("IntersectionObserver");
+            assignDummyMethods(this, "XMLHttpRequest", {
+              observe: undefined,
+              unobserve: undefined,
+              disconnect: undefined,
+            });
           };
 
-          Element.prototype.addEventListener = () => {
-            console.log("Pagefreeze Element.prototype.addEventListener");
-          };
-
-          window.document.addEventListener = () => {
-            console.log("Pagefreeze document.addEventListener");
-          };
-
-          // @ts-expect-error
-          window.fetch = () => {
-            console.log("Pagefreeze fetch");
-
-            return Promise.resolve();
-          };
-
-          // @ts-expect-error
-          window.XMLHttpRequest = class XMLHttpRequest {
-            constructor() {
-              console.log("Pagefreeze XMLHttpRequest");
-            }
-
-            open() {
-              console.log("Pagefreeze XMLHttpRequest.open");
-            }
-
-            setRequestHeader() {
-              console.log("Pagefreeze XMLHttpRequest.setRequestHeader");
-            }
-
-            send() {
-              console.log("Pagefreeze XMLHttpRequest.send");
-            }
-
-            addEventListener() {
-              console.log("Pagefreeze XMLHttpRequest.addEventListener");
-            }
-          };
-
-          // @ts-expect-error
-          window.IntersectionObserver = class IntersectionObserver {
-            constructor() {
-              console.log("Pagefreeze IntersectionObserver");
-            }
-
-            observe() {
-              console.log("Pagefreeze IntersectionObserver.observe");
-            }
-
-            unobserve() {
-              console.log("Pagefreeze IntersectionObserver.unobserve");
-            }
-
-            disconnect() {
-              console.log("Pagefreeze IntersectionObserver.disconnect");
-            }
-          };
-
-          // debugger;
-          // for (const key of Object.keys(Element.prototype)) {
-          //   let oldValue;
-          //   try {
-          //     oldValue = Element.prototype[key];
-          //   } catch (e) {
-          //     continue;
-          //   }
-          //   console.log(key, oldValue);
-          //   if (typeof oldValue === "function") {
-          //     Element.prototype[key] = function(...args) {
-          //       console.log(key, args);
-          //       return oldValue.call(this, ...args);
-          //     };
-          //   }
-          // }
-
-          // console.log(1111111111111);
           return 1;
         },
       },
       (...results) => {
-        // console.log(222222222222222, results);
+        // log(222222222222222, results);
       },
     );
   });
-  // chrome.contextMenus.create({
-  //   id: "sampleContextMenu",
-  //   title: "Sample Context Menu",
-  //   contexts: ["selection"],
-  // });
 });
-
-// const tabId = getTabId();
-// chrome.scripting.executeScript(
-//   {
-//     target: { tabId: tabId },
-//     files: ["script.js"],
-//   },
-//   () => {
-//     console.log(11111111111);
-//   },
-// );
