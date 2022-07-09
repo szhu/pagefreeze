c622ad5e24b310299d66238ffd2b06f842d7a5e5
Sean Zhu <interestinglythere@gmail.com>
2022-07-08 21:10:08 -0700

    Add disabled domains list

--- extension/page-background.js
+++ extension/page-background.js
@@ -6,13 +6,114 @@ function workerLog(...args) {
   console.info("Pagefreeze:", ...args);
 }
 
+class ExtStorage {
+  /**
+   * @param {chrome.storage.StorageArea} area
+   */
+  constructor(area) {
+    this.area = area;
+    /** @type {Record<string, any>} */
+    this.cache = {};
+  }
+
+  /**
+   * @param {Record<string, any>} keyMapping
+   */
+  get(keyMapping) {
+    let result = { ...keyMapping };
+    for (let key of Object.keys(keyMapping)) {
+      if (key in this.cache) {
+        result[key] = this.cache[key];
+      }
+    }
+    return result;
+  }
+
+  /**
+   * @template {{[key: string]: any}} T
+   * @param {T} keyMapping
+   */
+  async load(keyMapping) {
+    let result = await this.area.get(keyMapping);
+    this.cache = { ...this.cache, ...result };
+    return /** @type {T} */ (result);
+  }
+
+  /**
+   * @param {Record<string, any>} keyMapping
+   */
+  async save(keyMapping) {
+    this.cache = { ...this.cache, ...keyMapping };
+    await this.area.set(keyMapping);
+  }
+}
+
+const SyncStorage = new ExtStorage(chrome.storage.sync);
+workerLog(SyncStorage);
+
+async function toggleDomain(domain) {
+  let old = await SyncStorage.load({
+    disabledDomains: /** @type {string[]} */ ([]),
+  });
+  let disabledDomains = new Set(old.disabledDomains);
+  console.log(disabledDomains);
+
+  if (disabledDomains.has(domain)) {
+    workerLog("disabledDomains.delete", domain);
+    disabledDomains.delete(domain);
+  } else {
+    workerLog("disabledDomains.add", domain);
+    disabledDomains.add(domain);
+  }
+
+  await SyncStorage.save({ disabledDomains: [...disabledDomains] });
+}
+
+/**
+ * @param {number} tabId
+ * @param {string } url
+ */
+function updateBadge(tabId, url) {
+  let domain = new URL(url).origin;
+  if (SyncStorage.cache.disabledDomains.includes(domain)) {
+    chrome.action.setBadgeText({ tabId, text: "▶ JS" });
+  } else {
+    chrome.action.setBadgeText({ tabId, text: "" });
+    // chrome.action.setBadgeBackgroundColor({ tabId, color: "transparent" });
+  }
+}
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.id == null) return;
+  if (tab.url == null) return;
+
+  let domain = new URL(tab.url).origin;
+  await toggleDomain(domain);
+  updateBadge(tab.id, tab.url);
+});
+
 chrome.runtime.onInstalled.addListener(() => {
+  SyncStorage.load({
+    disabledDomains: /** @type {string[]} */ ([]),
+  });
+
   chrome.webNavigation.onCommitted.addListener((details) => {
     if (details.frameId !== 0) return;
 
     {
       let { tabId, frameId } = details;
       workerLog("chrome.webNavigation.onCommitted", { tabId, frameId });
+
+      updateBadge(details.tabId, details.url);
+      let domain = new URL(details.url).origin;
+      workerLog(
+        "Disabled for origin",
+        domain,
+        SyncStorage.cache.disabledDomains.includes(domain),
+      );
+      if (SyncStorage.cache.disabledDomains.includes(domain)) {
+        return;
+      }
     }
 
     chrome.scripting.executeScript(
@@ -49,7 +150,7 @@ chrome.runtime.onInstalled.addListener(() => {
             addEventListener: undefined,
             setTimeout: undefined,
             setInterval: undefined,
-            fetch: Promise.resolve(),
+            fetch: Promise.reject(),
           });
 
           assignDummyMethods(window.document, "document", {
